39d6a1b19ed34de9f56d7de40a599f5df296f753
Josh Green <josh@softwareverde.com>
2020-08-02 21:47:13 -0400

    Implemented basic request proxy.

---
+++ src/main/java/com/softwareverde/guvnor/BitcoinNodeAddress.java
@@ -0,0 +1,11 @@
+package com.softwareverde.guvnor;
+
+public class BitcoinNodeAddress {
+    public final String host;
+    public final Integer port;
+
+    public BitcoinNodeAddress(final String host, final Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+}
--- src/main/java/com/softwareverde/guvnor/Main.java
+++ src/main/java/com/softwareverde/guvnor/Main.java
@@ -1,14 +1,20 @@
 package com.softwareverde.guvnor;
 
+import com.softwareverde.constable.bytearray.ByteArray;
+import com.softwareverde.constable.bytearray.MutableByteArray;
+import com.softwareverde.constable.list.mutable.MutableList;
+import com.softwareverde.http.HttpRequest;
+import com.softwareverde.http.HttpResponse;
 import com.softwareverde.http.server.HttpServer;
 import com.softwareverde.http.server.endpoint.Endpoint;
 import com.softwareverde.http.server.servlet.Servlet;
+import com.softwareverde.http.server.servlet.request.Headers;
 import com.softwareverde.http.server.servlet.request.Request;
 import com.softwareverde.http.server.servlet.response.Response;
-import com.softwareverde.json.Json;
 import com.softwareverde.logging.LogLevel;
 import com.softwareverde.logging.Logger;
-import com.softwareverde.util.StringUtil;
+
+import java.util.Collection;
 
 public class Main {
     protected static Main INSTANCE = null;
@@ -17,7 +23,28 @@ public class Main {
         public static final Integer RPC_PORT = 8332;
     }
 
+    public static String getErrorMessage(final Integer responseCode) {
+        if (responseCode == null) {
+            return "send http request failed";
+        }
+
+        final int httpCode = responseCode;
+        if (httpCode == 0) {
+            return "couldn't connect to server";
+        }
+        else if (httpCode == Response.Codes.NOT_AUTHORIZED) {
+            return "Authorization failed: Incorrect rpcuser or rpcpassword";
+        }
+        else if ( (httpCode >= 400) && (httpCode != Response.Codes.BAD_REQUEST) && (httpCode != Response.Codes.NOT_FOUND) && (httpCode != Response.Codes.SERVER_ERROR) ) {
+            return ("server returned HTTP error " + responseCode);
+        }
+        else {
+            return "no response from server";
+        }
+    }
+
     protected final HttpServer _httpServer;
+    protected final MutableList<BitcoinNodeAddress> _bitcoinNodeAddresses = new MutableList<BitcoinNodeAddress>();
 
     public static void main(final String[] arguments) {
         if (Main.INSTANCE == null) {
@@ -29,17 +56,48 @@ public class Main {
     protected Main(final String[] arguments) {
         Logger.setLogLevel(LogLevel.ON);
 
+        { // TODO: Read from arguments...
+            _bitcoinNodeAddresses.add(new BitcoinNodeAddress("btc.sv.net", 8332));
+        }
+
         final Endpoint endpoint = new Endpoint(new Servlet() {
             @Override
             public Response onRequest(final Request request) {
-                final String postData = StringUtil.bytesToString(request.getRawPostData());
-                final Json json = Json.parse(postData);
+                final MutableByteArray rawPostData = MutableByteArray.wrap(request.getRawPostData());
 
-                Logger.trace(json.toFormattedString(2));
+                final BitcoinNodeAddress bitcoinNodeAddress = _bitcoinNodeAddresses.get(0);
+
+                final Integer proxiedResponseCode;
+                final ByteArray proxiedResult;
+                { // Proxy the request to the target node...
+                    final HttpRequest webRequest = new HttpRequest();
+                    webRequest.setUrl("http://" + bitcoinNodeAddress.host + ":" + bitcoinNodeAddress.port);
+                    webRequest.setAllowWebSocketUpgrade(false);
+                    webRequest.setFollowsRedirects(false);
+                    webRequest.setValidateSslCertificates(false);
+                    webRequest.setRequestData(rawPostData);
+                    webRequest.setMethod(request.getMethod());
+                    final Headers headers = request.getHeaders();
+                    for (final String key : headers.getHeaderNames()) {
+                        final Collection<String> values = headers.getHeader(key);
+                        for (final String value : values) {
+                            webRequest.setHeader(key, value);
+                        }
+                    }
+                    final HttpResponse proxiedResponse = webRequest.execute();
+
+                    proxiedResponseCode = proxiedResponse.getResponseCode();
+                    proxiedResult = proxiedResponse.getRawResult();
+                }
 
                 final Response response = new Response();
-                response.setCode(Response.Codes.BAD_REQUEST);
-                response.setContent(new byte[0]);
+                response.setCode(proxiedResponseCode);
+                if (proxiedResult != null) {
+                    response.setContent(proxiedResult.getBytes());
+                }
+                else {
+                    response.setContent(Main.getErrorMessage(proxiedResponseCode));
+                }
                 return response;
             }
         });
